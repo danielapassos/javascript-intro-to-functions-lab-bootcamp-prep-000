7699d51451cc099efcd988cb7a5d7568cd8f5a94
Daniela Passos <moitidani@gmail.com>
2018-06-27 12:55:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout (string) {
+  //receives an argument and returns it in all capslock
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  return string;
+}
+
+function logWhisper(string){
+  return string;
+}
+
+
+var upper = "HELLO!";
+
+function sayHiToGrandma(string){
+  (string.toUpperCase()) === (upper)
+    return "I can't hear you";
+    
+var lower = "hello!"
+  (string.toLowerCase()) === (lower)
+    return "YES, INDEED!";
+    
+var mixed = "I love you, Grandma."
+  (string.toUpperCase()) === (mixed)
+  (string.toLowerCase()) === (mixed)
+    return "I love you too!";
+}
