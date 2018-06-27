52489022c206a3dc6925d357ac1dbe49c6aed617
Daniela Passos <moitidani@gmail.com>
2018-06-27 13:40:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function whisper(string){
   return string.toLowerCase();
 }
 
-function logShout(string){
-  return string;
+function logShout (string) {
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string){
